b965ca153cda7f8e8964da6a02d73a4ca867cc0c
Riedler <git@riedler.wien>
2025-12-03 22:27:13 +0100

    fix: oops, forgot to rename an import

--- src/eduplanner_demo/populate.py
+++ src/eduplanner_demo/populate.py
@@ -1,1 +1,1 @@
-from config import Capability, Clazz, Course, Task, TaskStatus, User, read_moodle_config
+from eduplanner_demo.config import Capability, Clazz, Course, Task, TaskStatus, User, read_moodle_config
