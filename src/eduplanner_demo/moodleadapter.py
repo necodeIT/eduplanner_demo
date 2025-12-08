5eeebd070880cb98fe9c9f56e0bf8021e3de6f69
Riedler <git@riedler.wien>
2025-12-08 04:27:10 +0100

    feat: moodleadapter base structure

---
+++ src/eduplanner_demo/moodleadapter.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+from contextlib import contextmanager
+from typing import Iterator
+
+from .model import Task as mTask, User as mUser
+
+#
+# NOTE: MoodleAdapterOpen and MoodleAdapter are normally the same object,
+#       this is just structured this way for type checking purposes.
+#
+
+class MoodleAdapterOpen(ABC):
+	""" adapter to communicate with moodle - opened and ready for communication """
+	@abstractmethod
+	def clear_submissions(self) -> None:
+		""" clear all task submissions """
+		...
+
+	@abstractmethod
+	def set_submissions(self, tasks: list[mTask], user: mUser) -> None:
+		""" add user submissions to all listed tasks """
+		...
+
+	@abstractmethod
+	def clear_users(self) -> None:
+		""" clear all users with eduplanner roles """
+		...
+
+	@abstractmethod
+	def add_users(self, users: list[mUser]) -> None:
+		""" add users """
+		...
+
+
+class MoodleAdapterClosed(ABC):
+	""" adapter to communicate with moodle - closed and dormant """
+	@abstractmethod
+	@contextmanager
+	def connect(self) -> Iterator[MoodleAdapterOpen]:
+		""" enables safe cleanup via `with` statement """
+		...
+
+
+class MoodleAdapter(MoodleAdapterClosed, MoodleAdapterOpen):
+	...
--- src/eduplanner_demo/populate.py
+++ src/eduplanner_demo/populate.py
@@ -1,2 +1,15 @@
-from eduplanner_demo.model import Capability, Clazz, Course, Task, TaskStatus, User
-from eduplanner_demo.config import Config
+from .model import Capability, Clazz, Course, Task, TaskStatus, User
+from .config import Config
+from .moodleadapter import MoodleAdapterClosed
+
+def populate(adapter: MoodleAdapterClosed, config: Config) -> None:
+	""" resets moodle in terms of what eduplanner cares about """
+	with adapter.connect() as mdl:
+		# TODO: mdl.clear_submissions()
+		# TODO: mdl.clear_tasks()
+		# TODO: mdl.clear_plans()
+		# TODO: mdl.clear_courses()
+		mdl.clear_users()
+
+		# TODO: set courses and their accompanying data
+		# TODO: set users and their accompanying data
